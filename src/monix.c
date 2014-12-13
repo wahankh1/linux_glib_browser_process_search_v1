5dba9aac12b41c4d9033a7c5f17e0f2f77b67177
Wahankh <dev@wahankh.co.uk>
2014-12-13 16:25:06 +0000

    Find iceweasel pid
    	modified:   src/monix.c

--- src/monix.c
+++ src/monix.c
@@ -7,10 +7,6 @@
 
 gint main(gint argc, gchar **argv)
 {
-    #ifdef DEBUG 
-        debug("starting..."); 
-    #endif
-    
     g_type_init();
     
     search_process_directory("/proc");
@@ -32,7 +28,7 @@ void search_process_directory(const gchar *path)
     if(enumerator == NULL)
     {
         #ifdef DEBUG 
-            debug(error->message); 
+            //debug(error->message); 
         #endif
         
         g_error_free(error);
@@ -51,25 +47,25 @@ void search_process_directory(const gchar *path)
         type = g_file_info_get_file_type(info);
         name = g_file_info_get_name(info);
         
-        if(type == G_FILE_TYPE_REGULAR)
+        if(type == G_FILE_TYPE_REGULAR ||
+            !g_file_info_has_attribute(info, G_FILE_ATTRIBUTE_ACCESS_CAN_READ) ||
+            g_file_info_get_attribute_boolean(info, G_FILE_ATTRIBUTE_ACCESS_CAN_READ))
         {
+            // found status file
             if(g_strcmp0(name, "status") == 0)
             {
                 GIOChannel *gio;
                 GIOStatus status;
                 gchar *buffer;
                 gchar *status_file;
+                gboolean found = FALSE;
                 
                 status_file = g_strdup_printf("%s/%s", dir_path, name);
-                
-                #ifdef DEBUG 
-                    debug(status_file); 
-                #endif
                 
                 if((gio = g_io_channel_new_file(status_file, "r", &error)) == NULL)
                 {
                     #ifdef DEBUG 
-                        debug(error->message); 
+                        //debug(error->message); 
                     #endif
                     
                     g_error_free(error);
@@ -79,14 +75,30 @@ void search_process_directory(const gchar *path)
                 while((status = g_io_channel_read_line(gio, &buffer, NULL,NULL, &error)))
                 {
                     if(status == G_IO_STATUS_NORMAL)
-                    {
-                        #ifdef DEBUG 
-                            debug(buffer); 
-                        #endif
+                    {                        
+                        // contains string
+                        if(g_strstr_len(buffer, -1, "iceweasel") != NULL)
+                        {
+                            #ifdef DEBUG 
+                                debug(g_strdup_printf("Found Iceweasel -> %s", status_file)); 
+                            #endif
+                            
+                            found = TRUE;                            
+                        }
+                        
+                        if(g_strstr_len(buffer, -1, "Pid") != NULL && found)
+                        {
+                            #ifdef DEBUG 
+                                debug(g_strdup_printf("%s", buffer)); 
+                            #endif
+                            
+                            break;
+                        }
                     }
                     else
                     {
-                        g_io_channel_shutdown(gio, TRUE, &error);
+                        g_io_channel_shutdown(gio, FALSE, &error);
+                        g_io_channel_unref(gio);
                         
                         if(error != NULL)
                         {
@@ -96,6 +108,8 @@ void search_process_directory(const gchar *path)
                             
                             g_error_free(error);
                         }
+                        
+                        break;
                     }
                 }
                 
